aa26f4ab4b2e844b7cb7098ebdac94d4e17a95af
Yin Jun <yinjunok@foxmail.com>
2018-08-07 14:11:36 +0800

    解决构建后引用背景图路径错误

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -53,7 +53,7 @@ module.exports = {
             options: {
               // you can specify a publicPath here
               // by default it use publicPath in webpackOptions.output
-              // publicPath: 'asset/css/'
+              publicPath: '../../'
             }
           },
           {
